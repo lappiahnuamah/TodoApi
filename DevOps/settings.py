fb2d6050a6a7763b7ce9f6c90be055e5790383a2
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2024-06-19 11:45:28 +0000

    Configuring Server

--- DevOps/settings.py
+++ DevOps/settings.py
@@ -61,7 +61,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://0.0.0.0:8000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://127.0.0.1:8080"
+    "http://127.0.0.1:8080",
+    "http://52.91.6.114:8000/",
 ]
 
 ROOT_URLCONF = 'DevOps.urls'
