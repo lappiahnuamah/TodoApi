8857ffcb0491fee42429b19dcaf3cc29743fb1ff
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2024-06-28 22:46:58 +0000

    Changed

--- scripts/before_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker pull lappiahnuamah/devops-1:latest
-docker stop my-website || true
-docker rm my-website || true
--- scripts/start_server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d --name my-website -p 8000:8000 lappiahnuamah/devops-1:latest
